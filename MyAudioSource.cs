89ff4582c5d0936070263ddeba88826a2af44045
tatmos <cha@bb.wakwak.com>
2014-04-06 12:10:56 +0900

    WebPlayer時修正

--- MyAudioSource.cs
+++ MyAudioSource.cs
@@ -3,50 +3,79 @@ using System.Collections;
 #if UNITY_WEBPLAYER
 public class MyAudioSource : MonoBehaviour
 {	
-private AudioSource _Mysource;
-public AudioSource source 
-{
-	get
+	private AudioSource _Mysource;
+
+	public AudioSource source 
 	{
-		return _Mysource;
-	}
-}	
-public void Awake()
-{
-	if(_Mysource == null){
-		_Mysource = gameObject.AddComponent<AudioSource>();			
-		source.rolloffMode = AudioRolloffMode.Linear;	//3Dポジション時に音が小さくなるのを軽減
-	}
-}	
-#else	
-public class MyAudioSource : CriAtomSource
-{	
-	public CriAtomSource source { 
 		get
 		{
-			return this;
+			return _Mysource;
 		}
-	}
+	}	
 	public AudioClip clip{
 		set
 		{
-			this.cueName = value.name;
+			_Mysource.clip = value;
 		}
 	}
 	public bool isPlaying
 	{
 		get
 		{
-			if(this.status == Status.Playing)return true;
-			return false;
+			return _Mysource.isPlaying;
 		}
 	}
 	public float timeSamples
 	{
 		get
 		{
-			return this.time / 1000.0f * 44100f;
+			return _Mysource.timeSamples;
 		}
 	}
-	#endif		
+	public void Play()
+	{
+		_Mysource.Play();
+	}
+	public void Awake()
+	{
+		if(_Mysource == null){
+			_Mysource = gameObject.AddComponent<AudioSource>();			
+			source.rolloffMode = AudioRolloffMode.Linear;	//3Dポジション時に音が小さくなるのを軽減
+		}
+	}	
 }
+#else
+public class MyAudioSource : MonoBehaviour
+{	
+	public class MyAudioSource : CriAtomSource
+	{	
+		public CriAtomSource source { 
+			get
+			{
+				return this;
+			}
+		}
+		public AudioClip clip{
+			set
+			{
+				this.cueName = value.name;
+			}
+		}
+		public bool isPlaying
+		{
+			get
+			{
+				if(this.status == Status.Playing)return true;
+				return false;
+			}
+		}
+		public float timeSamples
+		{
+			get
+			{
+				return this.time / 1000.0f * 44100f;
+			}
+		}
+	}
+}
+#endif		
